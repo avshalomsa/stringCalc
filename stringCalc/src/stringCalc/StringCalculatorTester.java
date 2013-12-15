30ea91e9e25da04580db76d772c37eea6a6d40a3
avshalomsa <avshas22@gmail.com>
2013-12-15 21:52:20 +0200

    add tester

---
+++ stringCalc/src/stringCalc/StringCalculatorTester.java
@@ -0,0 +1,14 @@
+package stringCalc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
