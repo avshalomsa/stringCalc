d490df0f27be8e48b82227d023d0971eb42dde63
avshalomsa <avshas22@gmail.com>
2013-12-16 14:16:24 +0200

    add a boolean function isEmpty and add numbers string for the two value test

--- stringCalc/src/stringCalc/calculator.java
+++ stringCalc/src/stringCalc/calculator.java
@@ -3,15 +3,19 @@ package stringCalc;
 public class calculator {
 	  public int Add(String str) throws Exception
       {
-		  	
+		  	String[] numbers = str.split(",");
               if(str.isEmpty())
               {
                       return 0;
               }
-              return stringToInt(str);
-              
+              else {
+            	  return Integer.parseInt(numbers[0]+Integer.parseInt(numbers[1]));
+              }
       }
-
+	private boolean isEmpty(String str)
+	{
+		return str.isEmpty();
+	}
 	private int stringToInt(String str) {
 		// TODO Auto-generated method stub
 		return Integer.parseInt(str);
