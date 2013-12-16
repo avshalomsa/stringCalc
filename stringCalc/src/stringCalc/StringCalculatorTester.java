c23a81efd54649787014f4d4b11e3de4a8d3434d
avshalomsa <avshas22@gmail.com>
2013-12-16 15:06:48 +0200

    RED: check for difrrent spliters

--- stringCalc/src/stringCalc/StringCalculatorTester.java
+++ stringCalc/src/stringCalc/StringCalculatorTester.java
@@ -21,14 +21,41 @@ public class StringCalculatorTester {
       }
       
       @Test
-      public void testForOne() {
-            
-              try {
+      public void testForOne() throws Exception {
+ 
 				assertEquals(calc.Add("1"), 1);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		
+      }
+      @Test
+      public void TwoNumbers() throws Exception 
+      {
+              assertEquals(calc.Add("1,2"), 3);
+      }
+     
+      @Test
+      public void lineBetweenTwoNumbers() throws Exception
+      {
+              assertEquals(calc.Add("1\n3"), 4);
+      }
+      
+      @Test
+      public void threeNumRetunSum() throws Exception
+      {
+    	  assertEquals(calc.Add("1,3,3"), 7);
+      }
+      
+      @Test
+      public void lotOfNumbers() throws Exception
+      {
+              assertEquals(calc.Add("1,2,3,4,5"), 15);
+      }
+      
+      @Test
+      public void differentSpliters() throws Exception
+      {
+              assertEquals(calc.Add("//;\n1;2"), 3);
       }
 
+      
+      
 }
