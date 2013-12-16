06a6c34858cf211dc52646e7c023bce8a6e7ad80
avshalomsa <avshas22@gmail.com>
2013-12-16 13:36:30 +0200

    RED: try EmptyString fumction with "a" as a string

--- stringCalc/src/stringCalc/StringCalculatorTester.java
+++ stringCalc/src/stringCalc/StringCalculatorTester.java
@@ -2,13 +2,23 @@ package stringCalc;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTester {
+	  private calculator calc;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+      @Before
+      public void init()
+      {
+              calc=new calculator();
+      }
+
+      @Test 
+      public void EmptyString() throws Exception
+      {
+              calculator calc=new calculator();
+              assertEquals(calc.Add(""), 0);
+      }
 
 }
---
+++ stringCalc/src/stringCalc/calculator.java
@@ -0,0 +1,14 @@
+package stringCalc;
+
+public class calculator {
+	  public int Add(String str) throws Exception
+      {
+		  		str="r";
+              if(str.isEmpty())
+              {
+                      return 0;
+              }
+              return 1;
+              
+      }
+}
